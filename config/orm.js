98b88d2bab72eccaf0ea013cae0004c4c6bd896f
rperez2021 <rob.perez.flores@gmail.com>
2018-05-04 23:53:36 -0700

    Completed and working locally

--- config/orm.js
+++ config/orm.js
@@ -25,21 +25,3 @@ update: function(table, bool, col, name, callback) {
 }
 
 module.exports = orm;
-
-// var newf = {
-// 	// Function that returns all table entries
-// 	selectAll: function(tableInput, cb) {
-// 		// Construct the query string that returns all rows from the target table
-// 		var queryString = "SELECT * FROM " + tableInput + ";";
-
-// 		// Perform the database query
-// 		connection.query(queryString, function(err, result) {
-// 			if (err) {
-// 				throw err;
-// 			}
-
-// 			// Return results in callback
-// 			cb(result);
-// 		});
-//     }
-// }
