d711ce4e4ffb9039011b0178c225c98ebd12fcad
rperez2021 <rob.perez.flores@gmail.com>
2018-05-08 18:04:16 -0700

    Sequelized Version 5

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,6 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   ssl: true,
   dialect: 'postgres',
   protocol: 'postgres',
-  port: match[4],
-  host: match[3],
   logging: true
 });
 
