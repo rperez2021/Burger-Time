70d320bf4284da15a793d748ce0abee3db78ca4a
rperez2021 <rob.perez.flores@gmail.com>
2018-05-08 18:02:40 -0700

    Sequelized Version 5

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize({
-  connectionString: process.env.DATABASE_URL,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   ssl: true,
   dialect: 'postgres',
   protocol: 'postgres',
